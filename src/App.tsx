f750d8d81eff6524f80fc25a79dc7b4579d81850
neror <bitterhippo@gmail.com>
2025-09-28 20:42:40 -0700

    corrects small typescript error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-function App() {
-  return <>Responsitrillitrance.</>;
-}
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
-export default App;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
