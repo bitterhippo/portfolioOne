7731246fcbc7ac1449b70bcd56a6ad3816aacb33
neror <bitterhippo@gmail.com>
2025-10-05 21:41:33 -0700

    updates main.tsx to include a styled-components theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,20 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GlobalStyle } from "./GlobalStyles";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./components/Theme";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <GlobalStyle />
-        <App />
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <GlobalStyle />
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
